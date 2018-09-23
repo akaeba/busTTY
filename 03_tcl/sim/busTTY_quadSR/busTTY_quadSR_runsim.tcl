8a39c41db1fb5a80123d7a55a6a62fb83fda66ac
Andreas Kaeberlein <andreas.kaeberlein@web.de>
2018-09-23 20:54:03 +0200

    busTTY_quadSR_runsim.tcl -> added sim script

---
+++ 03_tcl/sim/busTTY_quadSR/busTTY_quadSR_runsim.tcl
@@ -0,0 +1,24 @@
+##************************************************************************
+## @copyright   LGPLv3
+## @author      akae
+##
+## @file        busTTY_quadSR_runsim.tcl
+## @note
+##
+## @brief       starts simulation
+## @details
+##
+## @date        2018-09-23
+## @version     0.1
+##************************************************************************
+
+
+
+# start simulation, disable optimization
+vsim -novopt -gDO_ALL_TEST=true work.busTTY_quadSR_tb
+
+# load Waveform
+do "../03_tcl/sim/busTTY_quadSR/busTTY_quadSR_waveform.do"
+
+# sim until finish
+run 7.5 us
